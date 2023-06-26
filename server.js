034b50540d5dfe36179de1b81310cc9285c8cd3b
PratyutCS <pratyut987@gmail.com>
2023-06-27 05:04:39 +0530

    forms page personalized

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ let dashboard = true;
 let forms = false;
 let form1 = true;
 let form2 = false;
+let form3 = false;
 
 require("dotenv").config()
 let Express = require('express')
@@ -24,6 +25,7 @@ app.set('view engine','ejs');
 app.set("views",path.join(__dirname, "./templates/views"))
 
 const urri = process.env.dburi;
+const secretKey = process.env.dbSessionKey;
 
 mongoose.connect(urri)
 .then((result)=>console.log("mdb 2 passed"))
@@ -51,7 +53,7 @@ const store = new mongodbsession({
 })
 
 app.use(session({
-    secret:"key that will protect the world",
+    secret:secretKey,
     resave:false,
     saveUninitialized:false,
     cookie: {
@@ -74,14 +76,14 @@ app.post("/index", async(req, res) => {
         let q=false,w=false;
         for(let i=0;i<email.length;i++){
             let ue=email.charCodeAt(i);
-            if(!((ue>=65 && ue<=90) || (ue>=97 && ue<=122))){
+            if(!((ue>=65 && ue<=90) || (ue>=97 && ue<=122) || (ue>=48 && ue<=57))){
                 q=true;
                 break;
             }
         }
         for(let i=0;i<pass.length;i++){
             let ue=pass.charCodeAt(i);
-            if(!((ue>=64 && ue<=90) || (ue>=97 && ue<=122))){
+            if(!((ue>=64 && ue<=90) || (ue>=97 && ue<=122) || (ue>=48 && ue<=57))){
                 w=true;
                 break;
             }
@@ -100,6 +102,7 @@ app.post("/index", async(req, res) => {
                 forms = false;
                 form1 = true;
                 form2 = false;
+                form3 = false;
                 req.session.isAuth="true";
                 req.session.username=useremail.title;
                 req.session.type=useremail.type;
@@ -125,21 +128,29 @@ app.post("/logout", (req, res) => {
 
 
 app.get("/dashboard", isAuth ,(req, res) => {
-    res.render('index',{type:req.session.type,
-                        name:req.session.username,
-                        dashboard:dashboard,
-                        forms:forms,
-                        form1:form1,
-                        form2:form2
+    res.render('index',{type : req.session.type,
+                        name : req.session.username,
+                        dashboard : dashboard,
+                        forms : forms,
+                        form1 : form1,
+                        form2 : form2,
+                        form3 : form3,
                     });
 })
 
 app.post("/dashdata", isAuth ,(req,res)=>{
-    if(typeof req.body.dashboard == "boolean" && typeof req.body.forms == "boolean" && typeof req.body.form1 == "boolean" && typeof req.body.form2 == "boolean"){
+    if(typeof req.body.dashboard == "boolean" && typeof req.body.forms == "boolean" && req.body.dashboard != undefined && req.body.forms != undefined && req.body.dashboard != req.body.forms){
         dashboard = req.body.dashboard;
         forms = req.body.forms;
+    }
+    res.redirect("/dashboard");
+})
+
+app.post("/formdata", isAuth ,(req,res)=>{
+    if(typeof req.body.form1 == "boolean" && typeof req.body.form2 == "boolean" && typeof req.body.form3 == "boolean" && req.body.form1 != undefined && req.body.form2 != undefined && req.body.form3 != undefined && req.body.form1!=req.body.form2!=req.body.form3){
         form1 = req.body.form1;
         form2 = req.body.form2;
+        form3 = req.body.form3;
     }
     res.redirect("/dashboard");
 })
