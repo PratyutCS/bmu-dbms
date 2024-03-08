b6428627cc32b9d314764ac9b64d19e6ee73d0bd
PratyutCS <pratyut987@gmail.com>
2024-03-08 22:01:21 +0530

    designing pages for various deparment

--- bmu-dbms-webpage/it/js/file.js
+++ bmu-dbms-webpage/it/js/file.js
@@ -7,28 +7,15 @@ function btn1(i){
     if(i==1){
         document.querySelector(".sc1-2-btn1").classList.add("sc1-2-btn-active");
         document.querySelector(".sc1-2-btn2").classList.remove("sc1-2-btn-active");
-        document.querySelector(".sc1-2-btn3").classList.remove("sc1-2-btn-active");
         document.querySelector(".sc1-2-txt1").style.color = "#000000";
         document.querySelector(".sc1-2-txt2").style.color = "#797979";
-        document.querySelector(".sc1-2-txt3").style.color = "#797979";
-        document.querySelector(".sc2-2-1-linkLine-0").innerHTML = "5.2.2";
-    }
-    else if(i==2){
-        document.querySelector(".sc1-2-btn1").classList.remove("sc1-2-btn-active");
-        document.querySelector(".sc1-2-btn2").classList.add("sc1-2-btn-active");
-        document.querySelector(".sc1-2-btn3").classList.remove("sc1-2-btn-active");
-        document.querySelector(".sc1-2-txt1").style.color = "#797979";
-        document.querySelector(".sc1-2-txt2").style.color = "#000000";
-        document.querySelector(".sc1-2-txt3").style.color = "#797979";
-        document.querySelector(".sc2-2-1-linkLine-0").innerHTML = "5.2.3";
+        document.querySelector(".sc2-2-1-linkLine-0").innerHTML = "4.3.2";
     }
     else{
         document.querySelector(".sc1-2-btn1").classList.remove("sc1-2-btn-active");
-        document.querySelector(".sc1-2-btn2").classList.remove("sc1-2-btn-active");
-        document.querySelector(".sc1-2-btn3").classList.add("sc1-2-btn-active");
+        document.querySelector(".sc1-2-btn2").classList.add("sc1-2-btn-active");
         document.querySelector(".sc1-2-txt1").style.color = "#797979";
-        document.querySelector(".sc1-2-txt2").style.color = "#797979";
-        document.querySelector(".sc1-2-txt3").style.color = "#000000";
-        document.querySelector(".sc2-2-1-linkLine-0").innerHTML = "5.4.1";
+        document.querySelector(".sc1-2-txt2").style.color = "#000000";
+        document.querySelector(".sc2-2-1-linkLine-0").innerHTML = "6.2.2";
     }
 }
