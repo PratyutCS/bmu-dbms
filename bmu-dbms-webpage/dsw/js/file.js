8a4efa055d0bd2d88de7916238c44feadc30e447
PratyutCS <pratyut987@gmail.com>
2024-03-08 21:49:15 +0530

    designing pages for various deparment

--- bmu-dbms-webpage/dsw/js/file.js
+++ bmu-dbms-webpage/dsw/js/file.js
@@ -8,27 +8,44 @@ function btn1(i){
         document.querySelector(".sc1-2-btn1").classList.add("sc1-2-btn-active");
         document.querySelector(".sc1-2-btn2").classList.remove("sc1-2-btn-active");
         document.querySelector(".sc1-2-btn3").classList.remove("sc1-2-btn-active");
+        document.querySelector(".sc1-2-btn4").classList.remove("sc1-2-btn-active");
         document.querySelector(".sc1-2-txt1").style.color = "#000000";
         document.querySelector(".sc1-2-txt2").style.color = "#797979";
         document.querySelector(".sc1-2-txt3").style.color = "#797979";
-        document.querySelector(".sc2-2-1-linkLine-0").innerHTML = "5.2.2";
+        document.querySelector(".sc1-2-txt4").style.color = "#797979";
+        document.querySelector(".sc2-2-1-linkLine-0").innerHTML = "3.6.2";
     }
     else if(i==2){
         document.querySelector(".sc1-2-btn1").classList.remove("sc1-2-btn-active");
         document.querySelector(".sc1-2-btn2").classList.add("sc1-2-btn-active");
         document.querySelector(".sc1-2-btn3").classList.remove("sc1-2-btn-active");
+        document.querySelector(".sc1-2-btn4").classList.remove("sc1-2-btn-active");
         document.querySelector(".sc1-2-txt1").style.color = "#797979";
         document.querySelector(".sc1-2-txt2").style.color = "#000000";
         document.querySelector(".sc1-2-txt3").style.color = "#797979";
-        document.querySelector(".sc2-2-1-linkLine-0").innerHTML = "5.2.3";
+        document.querySelector(".sc1-2-txt4").style.color = "#797979";
+        document.querySelector(".sc2-2-1-linkLine-0").innerHTML = "5.1.3";
+    }
+    else if(i==2){
+        document.querySelector(".sc1-2-btn1").classList.remove("sc1-2-btn-active");
+        document.querySelector(".sc1-2-btn2").classList.remove("sc1-2-btn-active");
+        document.querySelector(".sc1-2-btn3").classList.add("sc1-2-btn-active");
+        document.querySelector(".sc1-2-btn4").classList.remove("sc1-2-btn-active");
+        document.querySelector(".sc1-2-txt1").style.color = "#797979";
+        document.querySelector(".sc1-2-txt2").style.color = "#797979";
+        document.querySelector(".sc1-2-txt3").style.color = "#000000";
+        document.querySelector(".sc1-2-txt4").style.color = "#797979";
+        document.querySelector(".sc2-2-1-linkLine-0").innerHTML = "5.3.1";
     }
     else{
         document.querySelector(".sc1-2-btn1").classList.remove("sc1-2-btn-active");
         document.querySelector(".sc1-2-btn2").classList.remove("sc1-2-btn-active");
-        document.querySelector(".sc1-2-btn3").classList.add("sc1-2-btn-active");
+        document.querySelector(".sc1-2-btn3").classList.remove("sc1-2-btn-active");
+        document.querySelector(".sc1-2-btn4").classList.add("sc1-2-btn-active");
         document.querySelector(".sc1-2-txt1").style.color = "#797979";
         document.querySelector(".sc1-2-txt2").style.color = "#797979";
-        document.querySelector(".sc1-2-txt3").style.color = "#000000";
-        document.querySelector(".sc2-2-1-linkLine-0").innerHTML = "5.4.1";
+        document.querySelector(".sc1-2-txt3").style.color = "#797979";
+        document.querySelector(".sc1-2-txt4").style.color = "#000000";
+        document.querySelector(".sc2-2-1-linkLine-0").innerHTML = "5.3.3";
     }
 }
